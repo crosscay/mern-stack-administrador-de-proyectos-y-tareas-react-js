2e3d505f9f7f06cac02389ca1c574d16cbe0a2d3
crosscay <caicedocarlos0306@gmail.com>
2020-10-17 19:03:08 -0500

    cambios

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
 
@@ -7,14 +9,34 @@ const Login = () => {
         email: '',
         password: ''
     });
-    // const onChange = () => {
-    // }
+
+    // extraer de usuario
+    const { email, password } = usuario;
+
+    const onChange = (e) => {
+        e.preventDefault();
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    // Cuando el usuario quiere iniciar sesión
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // validar que no hayan campos vacios
+
+        // Pasarlo al action
+    }
 
     return (
         <div className="form-usuario">
             <div className="contenedor-form sombra-dark">
                 <h1>Iniciar Sesión</h1>
-                <forrm>
+                <forrm
+                    onSubmit={onSubmit}
+                >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input 
@@ -22,7 +44,8 @@ const Login = () => {
                             id="email"
                             name="email"
                             placeholder="email"
-                            // onChange={onChange}
+                            onChange={onChange}
+                            value={email}
                         />
                     </div>
                     <div className="campo-form">
@@ -32,10 +55,14 @@ const Login = () => {
                             id="password"
                             name="password"
                             placeholder="Tu Password"
-                            // onChange={onChange}
+                            onChange={onChange}
+                            value={password}
                         />
                     </div>
                 </forrm>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
             </div>
         </div>  
     );
