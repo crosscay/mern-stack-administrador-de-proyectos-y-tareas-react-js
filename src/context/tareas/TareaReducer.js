0ccacefc067f0ae842a7eaca5dfbcbb9d42caaa1
crosscay <caicedocarlos0306@gmail.com>
2021-01-31 21:32:13 -0500

    Agregando State y Provider para Tareas

---
+++ src/context/tareas/TareaReducer.js
@@ -0,0 +1,11 @@
+import { StaticRouter } from "react-router-dom"
+
+export default (state, action) => {
+
+    switch(action.type) {
+        
+        default: 
+            return state;
+
+    }
+}
